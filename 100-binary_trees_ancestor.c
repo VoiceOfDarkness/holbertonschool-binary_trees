1003d82ffb6c4a2c0b05a7ef7f2f0bf6f15e6526
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-18 23:05:47 -0800

    hot fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,35 +8,26 @@
  *
  * Return: pointer to common ancestor node.
  */
-
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *first_parent = NULL, *second_parent = NULL;
+    if (!first || !second)
+        return (NULL);
 
-	if (!first || !second)
-		return (NULL);
+    if (first->parent == second->parent)
+        return (first->parent);
 
-	first_parent = first->parent;
-	second_parent = second->parent;
+    if (first == second->parent)
+        return (second->parent);
 
-	if (first == second)
-		return ((binary_tree_t *)first);
+    if (second == first->parent)
+        return (first->parent);
 
-	if (first_parent == second_parent)
-		return (first_parent);
+    if (first->parent != second->parent)
+        return (binary_trees_ancestor(first, second->parent));
 
-	if (first_parent == second)
-		return (first_parent);
+    if (second->parent != first->parent)
+        return (binary_trees_ancestor(first->parent, second));
 
-	if (second_parent == first)
-		return (second_parent);
-
-	if (first_parent == NULL)
-		return (binary_trees_ancestor(first, second_parent));
-
-	if (second_parent == NULL)
-		return (binary_trees_ancestor(first_parent, second));
-
-	return (binary_trees_ancestor(first_parent, second_parent));
+    return (NULL);
 }
 
